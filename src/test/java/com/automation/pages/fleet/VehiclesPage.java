c315ff81d6507214d2dfa06f295306d2cef472a4
Huseyin Civan <huseyincivan@gmail.com>
2020-04-14 08:55:43 -0400

    updated

--- src/test/java/com/automation/pages/fleet/VehiclesPage.java
+++ src/test/java/com/automation/pages/fleet/VehiclesPage.java
@@ -3,75 +3,181 @@ package com.automation.pages.fleet;
 import com.automation.utilities.AbstractPageBase;
 import com.automation.utilities.BrowserUtils;
 import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 
-import java.util.ArrayList;
-import java.util.HashSet;
-import java.util.List;
-import java.util.Set;
+import java.util.*;
+//"1.Verify that truck driver should be able to see all Vehicle information once navigate to Vehicle page.
+//2.Verify that when user click on any car on the grid , system should display general information
+//about the car
+//3.Verify that truck driver can add Event and it should display under Activity tab and General
+//information page as well .
+//4.Verify that Truck driver can reset the Grid by click on Grid setting"
 
 public class VehiclesPage extends AbstractPageBase {
 
- @FindBy(css = "[class=\"column-manager dropdown\"]")
-  private WebElement gridSettingsBtn;
+    //****AC 1****//
+    @FindBy(css = "[class=\"column-manager dropdown\"]")
+    private WebElement gridSettingsBtn;
 
- @FindBy(linkText = "Select All")
- private WebElement selectAllBtn;
+    @FindBy(linkText = "Select All")
+    private WebElement selectAllBtn;
 
- @FindBy(xpath = "//tbody[@class=\"ui-sortable\"]/tr")
- private List<WebElement> vehicleInfoNames;
-
-
- @FindBy(css = "[class=\"grid-header\"]>tr>th[class^=\"grid\"]")
- private List<WebElement> vehicleInfoTableHeader;
+    @FindBy(xpath = "//tbody[@class=\"ui-sortable\"]/tr")
+    private List<WebElement> vehicleInfoNames;
 
 
- @FindBy(xpath = "//div[@class=\"dropdown-menu\"]//span[@class=\"close\"]")
- private WebElement closeBtn;
+    @FindBy(css = "[class=\"grid-header\"]>tr>th[class^=\"grid\"]")
+    private List<WebElement> vehicleInfoTableHeader;
 
 
- public void clickOnGridSettings(){
-     BrowserUtils.waitForPageToLoad(20);
-     wait.until(ExpectedConditions.elementToBeClickable(gridSettingsBtn)).click();
-
- }
-
- public void clickSelectAll(){
-     BrowserUtils.waitForPageToLoad(10);
-    wait.until(ExpectedConditions.elementToBeClickable(selectAllBtn)).click();
- }
+    @FindBy(xpath = "//div[@class=\"dropdown-menu\"]//span[@class=\"close\"]")
+    private WebElement closeBtn;
 
 
-    public Set<String > getAllVehicleInfoNamesFromGrid(){
+    //****AC 2 ****//
+    @FindBy(xpath = "//label[@class=\"dib\" and starts-with(text(), 'of')]")
+    private WebElement numberOfPages;
+
+    @FindBy(css = "[type=\"number\"]")
+    private WebElement pageNumberBox;
+
+//    @FindBy(css = "tbody>tr>td[class$='LicensePlate']")
+//    private List<WebElement> carsOnCurrentPage;
+
+    @FindBy(tagName = "h5")
+    private WebElement generalInfo;
+
+    //****AC 3 ****//
+
+    @FindBy(css = "[title=\"Add an event to this record\"]")
+    private WebElement addEventBtn;
+
+    @FindBy(css = "[id^=\"oro_calendar_event_form_title-uid\"]")
+    private WebElement titleTextBox;
+
+
+    @FindBy(xpath = "//*[text()=\"Save\"]")
+    private WebElement saveBtn;
+
+
+    @FindBy(xpath = "//*[@class=\"message-item message\"]")
+    private List<WebElement> eventTitlesOnActivity;
+
+
+//    @FindBy(xpath = "(//*[@class=\"message-item message\"])[1]")
+//    private WebElement lastCreatedEventTitle;
+
+    @FindBy(css = "[data-column-label=\"Title\"]")
+    private List<WebElement> allTitlesUnderCalenderEvent;
+
+
+    @FindBy(xpath = "//ul[@class=\"icons-holder\"]//*[contains(text(),\"Next\")]")
+    private WebElement nextBtnOnCalenderEvents;
+
+
+
+
+    //****AC 1****//
+    public void clickOnGridSettings() {
+        BrowserUtils.waitForPageToLoad(20);
+        wait.until(ExpectedConditions.elementToBeClickable(gridSettingsBtn)).click();
+    }
+
+    public void clickSelectAll() {
+        BrowserUtils.waitForPageToLoad(10);
+        wait.until(ExpectedConditions.elementToBeClickable(selectAllBtn)).click();
+    }
+
+    public Set<String> getAllVehicleInfoNamesFromGrid() {
         wait.until(ExpectedConditions.presenceOfElementLocated(By.xpath("//tbody[@class=\"ui-sortable\"]/tr")));
-       Set<String> allVehicleInfoNamesFromGrid= new HashSet<>();
+        Set<String> allVehicleInfoNamesFromGrid = new HashSet<>();
         for (WebElement vehicleInfoName : vehicleInfoNames) {
-            if(vehicleInfoName.getText().equals("Catalog Value (VAT Incl.)")){
+            if (vehicleInfoName.getText().equals("Catalog Value (VAT Incl.)")) {
                 allVehicleInfoNamesFromGrid.add("CVVI");
-            }else{
+            } else {
                 allVehicleInfoNamesFromGrid.add(vehicleInfoName.getText().toUpperCase());
             }
         }
         return allVehicleInfoNamesFromGrid;
     }
 
+    public void clickClose() {
+        wait.until(ExpectedConditions.elementToBeClickable(closeBtn)).click();
+    }
 
-
- public void clickClose(){
-     wait.until(ExpectedConditions.elementToBeClickable(closeBtn)).click();
- }
-
-
-
-    public Set<String> getVehicleInfoFromTableHeader(){
+    public Set<String> getVehicleInfoFromTableHeader() {
         wait.until(ExpectedConditions.presenceOfElementLocated(By.cssSelector("[class=\"grid-header-row\"]>th[class^=\"grid-cell grid-header-cell grid-header-cell-\"]")));
-        Set<String> allVehicleInfoNamesFromHeader=new HashSet<>();
+        Set<String> allVehicleInfoNamesFromHeader = new HashSet<>();
         for (WebElement element : vehicleInfoTableHeader) {
             allVehicleInfoNamesFromHeader.add(element.getText().toUpperCase());
         }
         return allVehicleInfoNamesFromHeader;
     }
+
+
+    //****AC 2****//
+    public void randomlySelectACar() {
+        int totalPageNumber = Integer.parseInt(numberOfPages.getText().split(" ")[1]);
+        //  System.out.println("Total pageNumber = " + totalPageNumber);
+        Random random = new Random();
+        int pageNum = random.nextInt(totalPageNumber) + 1;
+        //  System.out.println(pageNum);
+
+        JavascriptExecutor js = (JavascriptExecutor) driver;
+        js.executeScript("document.querySelector('[type=\"number\"]').setAttribute('value', '')");
+
+        pageNumberBox.sendKeys(pageNum + "", Keys.ENTER);
+
+        BrowserUtils.waitForPageToLoad(10);
+        BrowserUtils.wait(1);
+
+        List<WebElement> carsOnCurrentPage = driver.findElements(By.cssSelector("tbody>tr>td[class$='LicensePlate']"));
+        int carIndexNum = random.nextInt(carsOnCurrentPage.size());
+
+        wait.until(ExpectedConditions.elementToBeClickable(carsOnCurrentPage.get(carIndexNum)));
+
+        carsOnCurrentPage.get(carIndexNum).click();
+
+    }
+
+    public String getInfoText() {
+        BrowserUtils.waitForPageToLoad(10);
+        wait.until(ExpectedConditions.presenceOfElementLocated(By.tagName("h5")));
+        return generalInfo.getText();
+    }
+
+    //****AC 3****//
+
+    public void clickAddEvent(){
+        BrowserUtils.waitForPageToLoad(10);
+        wait.until(ExpectedConditions.elementToBeClickable(addEventBtn)).click();
+    }
+
+    public void sendTextToTitle(String title){
+        BrowserUtils.waitForPageToLoad(10);
+        wait.until(ExpectedConditions.elementToBeClickable(titleTextBox)).sendKeys(title);
+
+    }
+
+    public void clickSaveBtn(){
+        BrowserUtils.waitForPageToLoad(10);
+        wait.until(ExpectedConditions.elementToBeClickable(saveBtn)).click();
+    }
+
+    public List<String > getTitlesOfActivity(){
+        BrowserUtils.waitForPageToLoad(10);
+BrowserUtils.wait(3);
+        List<String > titlesOfActivities=new ArrayList<>();
+        for (int x = 0; x < eventTitlesOnActivity.size() ; x++) {
+            titlesOfActivities.add(eventTitlesOnActivity.get(x).getText());
+        }
+        return titlesOfActivities;
+    }
+
+
 
 }
--- src/test/java/com/automation/test/fleet/VehicleTests.java
+++ src/test/java/com/automation/test/fleet/VehicleTests.java
@@ -1,8 +1,10 @@
 package com.automation.test.fleet;
 
+
 import com.automation.pages.fleet.VehiclesPage;
 import com.automation.pages.login.LoginPage;
 import com.automation.utilities.AbstractTestBase;
+import com.automation.utilities.BrowserUtils;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
@@ -17,28 +19,79 @@ public void verifyAllVehicleInformations(){
         test=report.createTest("Verify All Vehicle Information");
         LoginPage loginPage=new LoginPage();
         VehiclesPage vehiclesPage=new VehiclesPage();
-        test.info("Login as store manager");
+        test.info("Login as truck driver");
 
-        loginPage.login("user1","UserUser123");
+        loginPage.login();
         vehiclesPage.navigateTo("Fleet","Vehicles");
         vehiclesPage.clickOnGridSettings();
         vehiclesPage.clickSelectAll();
 
         Set<String > expected =vehiclesPage.getAllVehicleInfoNamesFromGrid();
-            for (String s : expected) {
-                    System.out.println(s);
-            }
+  //          for (String s : expected) {
+  //                  System.out.println(s);
+  //          }
 
-        System.out.println("##########################################################");
+  //      System.out.println("##########################################################");
         Set<String > actual=vehiclesPage.getVehicleInfoFromTableHeader();
-            for (String s : actual) {
-                    System.out.println(s);
-            }
+  //          for (String s : actual) {
+  //                  System.out.println(s);
+  //          }
 
        Assert.assertEquals(actual,expected);
 
       test.pass("All Vehicle Information were verified");
 
+
+
+
+}
+@Test
+public void verifyInfoText(){
+
+    test=report.createTest("Verify General Info Text");
+    LoginPage loginPage=new LoginPage();
+    VehiclesPage vehiclesPage=new VehiclesPage();
+    test.info("Login as truck driver");
+
+    loginPage.login("user1","UserUser123");
+    vehiclesPage.navigateTo("Fleet","Vehicles");
+
+
+    vehiclesPage.randomlySelectACar();
+    String actualInfoMessage = vehiclesPage.getInfoText();
+    String expectedINfoMessage = "General Information";
+
+    Assert.assertEquals(actualInfoMessage,expectedINfoMessage);
+}
+
+@Test
+    public void verifyDriverAddEvent(){
+
+    test=report.createTest("Verify Driver add event");
+    LoginPage loginPage=new LoginPage();
+    VehiclesPage vehiclesPage=new VehiclesPage();
+    test.info("Login as truck driver");
+
+    loginPage.login("user1","UserUser123");
+    vehiclesPage.navigateTo("Fleet","Vehicles");
+
+
+    vehiclesPage.randomlySelectACar();
+    vehiclesPage.clickAddEvent();
+
+    String testTitle ="test_101";
+    vehiclesPage.sendTextToTitle(testTitle);
+    vehiclesPage.clickSaveBtn();
+
+//    List<String > activityTitles = vehiclesPage.getTitlesOfActivity();
+//    for (String activityTitle : activityTitles) {
+//        System.out.println(activityTitle);
+//    }
+  //  System.out.println(vehiclesPage.getLastEventTitle());
+
+   Assert.assertTrue(vehiclesPage.getTitlesOfActivity().contains(testTitle));
+
+   vehiclesPage.navigateTo("Activities","Calendar Events");
 
 
 
